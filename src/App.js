db38cec82d5e4f6977897b2b6767f7b7912e1b6b
Natalia <nwisely@amazon.id>
2021-07-08 22:47:17 +0700

    Breaking down parts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import Amplify, { Auth } from "aws-amplify";
 import "./App.css";
@@ -8,111 +8,8 @@ import home from "./home.js";
 import Location from "aws-sdk/clients/location";
 import awsconfig from "./aws-exports";
 
-const AWS = require("aws-sdk");
-AWS.config.region = "RegionToUse"; // Region
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: "us-east-2:a7c0847c-d830-4bd4-8cfd-665bb65b8fbe",
-});
-// Make the call to obtain credentials
-AWS.config.credentials.get(function () {
-  // Credentials will be available when this function is called.
-  var accessKeyId = AWS.config.credentials.accessKeyId;
-  var secretAccessKey = AWS.config.credentials.secretAccessKey;
-  var sessionToken = AWS.config.credentials.sessionToken;
-});
 Amplify.configure(awsconfig);
-
-const bucket = "awsbucketwise"; // the bucketname without s3://
-const photo_source = "1.png";
-const photo_target = "S1.png";
-const config = new AWS.Config({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
-const client = new AWS.Rekognition();
-const params = {
-  SourceImage: {
-    S3Object: {
-      Bucket: bucket,
-      Name: photo_source,
-    },
-  },
-  TargetImage: {
-    S3Object: {
-      Bucket: bucket,
-      Name: photo_target,
-    },
-  },
-  SimilarityThreshold: 70,
-};
-client.compareFaces(params, function (err, response) {
-  if (err) {
-    console.log(err, err.stack); // an error occurred
-  } else {
-    response.FaceMatches.forEach((data) => {
-      let position = data.Face.BoundingBox;
-      let similarity = data.Similarity;
-      console.log(
-        `The face at: ${position.Left}, ${position.Top} matches with ${similarity} % confidence`
-      );
-    }); // for response.faceDetails
-  } // if
-});
-
-const createClient = async () => {
-  const credentials = await Auth.currentCredentials();
-  const client = new Location({
-    credentials,
-    region: awsconfig.aws_project_region,
-  });
-  const params = {
-    IndexName: "MyPlaceIndex",
-    Text: "Gading Residence Jakarta",
-  };
-  client.searchPlaceIndexForText(params, (err, data) => {
-    if (err) console.error(err);
-    if (data) console.log(data.Results[0].Place);
-  });
-
-  // Creating a promise out of the function
-  let getLocationPromise = new Promise((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        resolve({ latitude: lat, longitude: long });
-      });
-    } else {
-      reject("Your browser doesn't support geolocation API");
-    }
-  });
-  const coord = {
-    IndexName: "MyPlaceIndex",
-    Position: [10, 20],
-  };
-
-  getLocationPromise
-    .then((location) => {
-      coord.Position = [location.latitude, location.longitude];
-      console.log(location);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  // getting address from coordinates
-
-  client.searchPlaceIndexForPosition(coord, (err, data) => {
-    if (err) console.error(err);
-    console.log("C", coord);
-    if (data) console.log("coord", data);
-  });
-  return client;
-};
-
 const App = () => {
-  createClient();
   return (
     <BrowserRouter>
       <div className="Background">
--- src/home.js
+++ src/home.js
@@ -1,10 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon, InlineIcon } from "@iconify/react";
 import outlineFileUpload from "@iconify/icons-ic/outline-file-upload";
 import uploadImg from "./icon.png";
 import { NavLink } from "react-router-dom";
+import Location from "aws-sdk/clients/location";
+import awsconfig from "./aws-exports";
+import Amplify, { Auth } from "aws-amplify";
+import ImageUploader from "react-images-upload";
 
 export default function thePageName() {
+  const changeHandler = (e) => {
+    console.log("hello");
+    console.log(e);
+  };
+
+  const AWS = require("aws-sdk");
+  AWS.config.region = "us-east-2"; // Region
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "us-east-2:a7c0847c-d830-4bd4-8cfd-665bb65b8fbe",
+  });
+  // Make the call to obtain credentials
+  AWS.config.credentials.get(function () {
+    // Credentials will be available when this function is called.
+    var accessKeyId = AWS.config.credentials.accessKeyId;
+    var secretAccessKey = AWS.config.credentials.secretAccessKey;
+    var sessionToken = AWS.config.credentials.sessionToken;
+  });
+
+  const S3_BUCKET = "awsbucketwise";
+  const REGION = "us-east-2";
+  const myBucket = new AWS.S3({
+    params: { Bucket: S3_BUCKET },
+    region: REGION,
+  });
+
+  const bucket = "awsbucketwise"; // the bucketname without s3://
+  const photo_source = "1.png";
+  const photo_target = "S1.png";
+  const client = new AWS.Rekognition();
+  const params = {
+    SourceImage: {
+      S3Object: {
+        Bucket: bucket,
+        Name: photo_source,
+      },
+    },
+    TargetImage: {
+      S3Object: {
+        Bucket: bucket,
+        Name: photo_target,
+      },
+    },
+    SimilarityThreshold: 80,
+  };
+  client.compareFaces(params, function (err, response) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    } else {
+      response.FaceMatches.forEach((data) => {
+        let position = data.Face.BoundingBox;
+        let similarity = data.Similarity;
+        console.log(
+          `The face at: ${position.Left}, ${position.Top} matches with ${similarity} % confidence`
+        );
+      }); // for response.faceDetails
+    } // if
+  });
+
+  const createClient = async () => {
+    const credentials = await Auth.currentCredentials();
+    const client = new Location({
+      credentials,
+      region: awsconfig.aws_project_region,
+    });
+    const params = {
+      IndexName: "MyPlaceIndex",
+      Text: "Gading Residence Jakarta", //Need to be retrieved from S3 Text Rekognition
+    };
+    client.searchPlaceIndexForText(params, (err, data) => {
+      if (err) console.error(err);
+      if (data) console.log(data.Results[0].Place);
+    });
+
+    // Creating a promise out of the function
+    let getLocationPromise = new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          var lat = position.coords.latitude;
+          var long = position.coords.longitude;
+          resolve({ latitude: lat, longitude: long });
+        });
+      } else {
+        reject("Your browser doesn't support geolocation API");
+      }
+    });
+    const coord = {
+      IndexName: "MyPlaceIndex",
+      Position: [10, 20],
+    };
+
+    getLocationPromise
+      .then((location) => {
+        coord.Position = [location.latitude, location.longitude];
+        console.log(location);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // getting address from coordinates
+
+    client.searchPlaceIndexForPosition(coord, (err, data) => {
+      if (err) console.error(err);
+      console.log("C", coord);
+      if (data) console.log("coord", data.Results[0].Place["Country"]); // Current country
+    });
+    return client;
+  };
+
+  createClient();
   return (
     <div className="Background">
       <div className="bCircle"> </div>
@@ -12,8 +126,9 @@ export default function thePageName() {
         <img className="uploadPic" src={uploadImg} />
       </div>
 
-      <NavLink to="/locateuser" onClick={console.log("HELLO")}>
-        <button className="ubutton">
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span className="ubutton">
           <div className="b_inside">
             <Icon
               icon={outlineFileUpload}
@@ -35,9 +150,8 @@ export default function thePageName() {
               Upload
             </p>
           </div>
-        </button>
-      </NavLink>
-
+        </span>
+      </label>
       <div className="Bar">
         <div className="Bar2"></div>
         <div className="contents">
